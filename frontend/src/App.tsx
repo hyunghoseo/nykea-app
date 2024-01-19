c526f5f2e438643e795562738dc95568ba817fd9
Hyungho Seo <hyunghoseo@gmail.com>
2024-01-19 10:19:33 -0500

    KEA-87 configure linking

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,27 @@
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  LinkingOptions,
+  NavigationContainer,
+  ParamListBase,
+} from "@react-navigation/native";
 import { View, StyleSheet } from "react-native";
 import Header from "./components/Header/Header";
 import AppNavigator from "./navigation/AppNavigator";
 import { registerRootComponent } from "expo";
 
+const linking: LinkingOptions<ParamListBase> = {
+  prefixes: ["nykea.org", "nykea://"],
+  config: {
+    screens: {
+      Home: "home",
+      About: "about",
+      // More screens...
+    },
+  },
+};
+
 const App = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={linking}>
       <View style={styles.container}>
         <Header />
         <View style={styles.screenContainer}>
