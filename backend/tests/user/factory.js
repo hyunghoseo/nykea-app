eed69a975d2dd2430aebf1b2636c61fd792b6b44
jiahn326 <82917839+jiahn326@users.noreply.github.com>
2024-01-18 19:39:27 -0800

    Kea-140 KEA-109 Merge (#29)
    * KEA-109 updated attributes
    
    * KEA-109 testing environment setup
    
    * KEA-109 updated readme
    
    * KEA-109 auth controller test
    
    * KEA-109 testing endpoint
    
    * KEA-109 added swagger
    
    * KEA-109 app key fix
    
    * KEA-109 config sync
    
    * KEA-109 Admin role created
    
    * KEA-109 test code
    
    * KEA-109 find test
    
    * KEA-109 fix pipeline
    
    * KEA-109 config sync
    
    * KEA-109 add config sync during test
    
    * KEA-109 Update backend.yml
    
    * KEA-109 cs
    
    * KEA-109 Update backend.yml
    
    * KEA-109 Update backend.yml
    
    * KEA-109 Update backend.yml
    
    * KEA-109 Update backend.yml
    
    * KEA-109 token salt
    
    * KEA-109 token salt
    
    * KEA-109 Update backend.yml
    
    * KEA-109 Update frontend.yml
    
    ---------
    
    Co-authored-by: KyungminUm <snownaul2@gmail.com>
    Co-authored-by: doolri1276 <doolri1277@gmail.com>
    Co-authored-by: doolri1276 <31979615+doolri1276@users.noreply.github.com>

---
+++ backend/tests/user/factory.js
@@ -0,0 +1,50 @@
+/**
+ * Default data that factory use
+ */
+const defaultData = {
+    password: "1234Abc",
+    provider: "local",
+    confirmed: true,
+};
+/**
+ * Returns random username object for user creation
+ * @param {object} options that overwrites default options
+ * @returns {object} object that is used with `strapi.plugins["users-permissions"].services.user.add`
+ */
+const mockUserData = (options = {}) => {
+    const usernameSuffix = Math.round(Math.random() * 10000).toString();
+    return {
+        username: `tester${usernameSuffix}`,
+        email: `tester${usernameSuffix}@strapi.com`,
+        ...defaultData,
+        ...options,
+    };
+    return;
+};
+/**
+ * Creates new user in strapi database
+ * @param strapi, instance of strapi
+ * @param options that overwrites default options
+ * @returns {object} object of new created user, fetched from database
+ */
+const createUser = async (strapi, role, data) => {
+    /** Gets the default user role */
+    const defaultRole = await strapi
+        .db.query("plugin::users-permissions.role")
+        .findOne({
+            where: {
+                id: role
+            }
+        }, []);
+    /** Creates a new user an push to database */
+    return await strapi.plugins["users-permissions"].services.user.add({
+        ...(data || mockUserData()),
+        role: defaultRole ? defaultRole.id : null,
+    });
+};
+
+module.exports = {
+    mockUserData,
+    createUser,
+    defaultData,
+};
