faf7e2061bf9a625b9bc783fede0f8ca9f80496a
Calvin Yoon <calvinxproject@gmail.com>
2024-02-04 20:51:35 -0500

    started test code & set admin permissions for church-info strapi

--- backend/tests/api/church-info/index.js
+++ backend/tests/api/church-info/index.js
@@ -1,1 +1,35 @@
 // create church-info index file
+
+const request = require('supertest');
+const { jwt } = require("../../helpers/strapi");
+const userFactory = require("../../user/factory");
+
+describe("Church Info Test", () => {
+    let authenticatedUser;
+    let adminUser;
+    beforeAll(async () => {
+        console.log("Church Info before all");
+        authenticatedUser = await userFactory.createUser(strapi, 'authenticated');
+        adminUser = await userFactory.createUser(strapi, "admin");
+    });
+
+    /**
+     * @param {string | number} id
+     */
+
+    const constructChurchInfo = (id) => {
+        return {
+            data: {
+                "ChurchName": "Test Name " + id,
+                "Phone": "000-000-0000" + id,
+                "Email": "test" + id + "@test.com",
+                "Address": id + "north haledon",
+                "PrivatePolicy": "policy" + id,
+                "locale": "en",
+                "publishedAt": Date.now(),
+            }
+        };
+    }
+
+
+})
