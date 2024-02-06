84b64b686697f0fb3aeed04b7ed5f4da4c253142
Calvin Yoon <calvinxproject@gmail.com>
2024-02-06 14:14:49 -0500

    drafted tests for church-info

--- backend/tests/api/church-info/index.js
+++ backend/tests/api/church-info/index.js
@@ -1,5 +1,3 @@
-// create church-info index file
-
 const request = require('supertest');
 const { jwt } = require("../../helpers/strapi");
 const userFactory = require("../../user/factory");
@@ -9,7 +7,7 @@ describe("Church Info Test", () => {
     let adminUser;
     beforeAll(async () => {
         console.log("Church Info before all");
-        authenticatedUser = await userFactory.createUser(strapi, 'authenticated');
+        authenticatedUser = await userFactory.createUser(strapi, "authenticated");
         adminUser = await userFactory.createUser(strapi, "admin");
     });
 
@@ -31,5 +29,91 @@ describe("Church Info Test", () => {
         };
     }
 
+    it("Public user should not put church-info", async () => {
+        await request(strapi.server.httpServer)
+            .put("/api/church-info") //strapi uses put for single type
+            .set("accept", "application/json")
+            .send(constructChurchInfo(0))
+            .expect("Content-Type", /json/)
+            .expect(500)
+    });
 
+    it("Authenticated user should not put church-info", async () => {
+        await request(strapi.server.httpServer)
+            .put("/api/church-info") //strapi uses put for single type
+            .set("accept", "application/json")
+            .set("Authorization", `Bearer ${await jwt(authenticatedUser.id)}`)
+            .send(constructChurchInfo(0))
+            .expect("Content-Type", /json/)
+            .expect(500)
+    });
+
+    it("Admin user should put church-info", async () => {
+        await request(strapi.server.httpServer)
+            .put("/api/church-info") //strapi uses put for single type
+            .set("accept", "application/json")
+            .set("Authorization", `Bearer ${await jwt(adminUser.id)}`)
+            .send(constructChurchInfo(1))
+            .expect("Content-Type", /json/)
+            .expect(200)   
+    });
+
+    it("Public user should find church-info", async () => {
+        const id = 1;
+        const churchInfo = constructChurchInfo(id);
+        await request(strapi.server.httpServer)
+            .get("/api/church-info")
+            .set("accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .then((data) => {
+                data = data.body.data;
+                expect(data.attributes.ChurchName).toBe(churchInfo.data.ChurchName);
+                expect(data.attributes.Phone).toBe(churchInfo.data.Phone);
+                expect(data.attributes.Email).toBe(churchInfo.data.Email);
+                expect(data.attributes.Address).toBe(churchInfo.data.Address);
+                expect(data.attributes.PrivatePolicy).toBe(churchInfo.data.PrivatePolicy);
+                expect(data.attributes.locale).toBe(churchInfo.data.locale);
+            });
+    });
+
+    it("Authenticated user should find church-info", async () => {
+        const id = 1;
+        const churchInfo = constructChurchInfo(id);
+        await request(strapi.server.httpServer)
+            .get("/api/church-info")
+            .set("accept", "application/json")
+            .set("Authorization", `Bearer ${await jwt(authenticatedUser.id)}`)
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .then((data) => {
+                data = data.body.data;
+                expect(data.attributes.ChurchName).toBe(churchInfo.data.ChurchName);
+                expect(data.attributes.Phone).toBe(churchInfo.data.Phone);
+                expect(data.attributes.Email).toBe(churchInfo.data.Email);
+                expect(data.attributes.Address).toBe(churchInfo.data.Address);
+                expect(data.attributes.PrivatePolicy).toBe(churchInfo.data.PrivatePolicy);
+                expect(data.attributes.locale).toBe(churchInfo.data.locale);
+            });
+    });
+
+    it("Admin user should find church-info", async () => {
+        const id = 1;
+        const churchInfo = constructChurchInfo(id);
+        await request(strapi.server.httpServer)
+            .get("/api/church-info")
+            .set("accept", "application/json")
+            .set("Authorization", `Bearer ${await jwt(adminUser.id)}`)
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .then((data) => {
+                data = data.body.data;
+                expect(data.attributes.ChurchName).toBe(churchInfo.data.ChurchName);
+                expect(data.attributes.Phone).toBe(churchInfo.data.Phone);
+                expect(data.attributes.Email).toBe(churchInfo.data.Email);
+                expect(data.attributes.Address).toBe(churchInfo.data.Address);
+                expect(data.attributes.PrivatePolicy).toBe(churchInfo.data.PrivatePolicy);
+                expect(data.attributes.locale).toBe(churchInfo.data.locale);
+            });
+    });
 })
