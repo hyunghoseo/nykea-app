6534bc7d3e123b6bcea72182d81b44aa719f8eb3
Calvin Yoon <calvinxproject@gmail.com>
2024-02-06 18:58:19 -0500

    created test index.js for support

--- backend/tests/api/support/index.js
+++ backend/tests/api/support/index.js
@@ -14,7 +14,11 @@ describe("Support Test", () => {
     /**
      * @param {string | number} id
      */
+<<<<<<< HEAD
     const constructSupportTicket = (id) => {
+=======
+    const constructGroup = (id) => {
+>>>>>>> afbec58 (created test index.js for support)
         return {
             data: {
                 "FullName": "Test Name" + id,
@@ -23,14 +27,21 @@ describe("Support Test", () => {
                 "Title": "Test Title" + id,
                 "Message": "Test Message" + id,
                 "DateSubmitted": Date.now(),
+<<<<<<< HEAD
                 "Status": "Submitted", // "Under Review" or "Completed"
                 "Memo": null,
                 "DateCompleted": null,
+=======
+                "Status": "Submitted", //"Under Review" or "Completed"
+                "Memo": "Test Memo" + id,
+                "DateCompleted": Date.now(),
+>>>>>>> afbec58 (created test index.js for support)
                 "locale": "en"
             }
         };
     }
 
+<<<<<<< HEAD
     it("Public user should post support ticket", async () => {
         await request(strapi.server.httpServer)
             .post("/api/supports")
@@ -141,4 +152,131 @@ describe("Support Test", () => {
             });
     });
         
+=======
+
+    
+
+
+    // it("Public user should not post group", async () => {
+    //     await request(strapi.server.httpServer)
+    //         .post("/api/groups")
+    //         .set("accept", "application/json")
+    //         .send(constructGroup(0))
+    //         .expect("Content-Type", /json/)
+    //         .expect(500)
+    // })
+
+    // it("Authenticated user should not post group", async () => {
+    //     await request(strapi.server.httpServer)
+    //         .post("/api/groups")
+    //         .set("accept", "application/json")
+    //         .set("Authorization", `Bearer ${await jwt(authenticatedUser.id)}`)
+    //         .send(constructGroup(0))
+    //         .expect("Content-Type", /json/)
+    //         .expect(500)
+    // })
+
+    // it("Admin user should post groups", async () => {
+    //     await request(strapi.server.httpServer)
+    //         .post("/api/groups")
+    //         .set("accept", "application/json")
+    //         .set("Authorization", `Bearer ${await jwt(adminUser.id)}`)
+    //         .send(constructGroup(1))
+    //         .expect("Content-Type", /json/)
+    //         .expect(200)
+
+    //     await request(strapi.server.httpServer)
+    //         .post("/api/groups")
+    //         .set("accept", "application/json")
+    //         .set("Authorization", `Bearer ${await jwt(adminUser.id)}`)
+    //         .send(constructGroup(1))
+    //         .expect("Content-Type", /json/)
+    //         .expect(200)
+    // })
+
+    // it("Public user should find groups", async () => {
+    //     await request(strapi.server.httpServer)
+    //         .get("/api/groups")
+    //         .set("accept", "application/json")
+    //         .expect("Content-Type", /json/)
+    //         .expect(200)
+    //         .then((data) => {
+    //             expect(data.body.data.length).toBe(2);
+    //         });
+    // });
+
+    // it("Authenticated user should find groups", async () => {
+    //     await request(strapi.server.httpServer)
+    //         .get("/api/groups")
+    //         .set("accept", "application/json")
+    //         .set("Authorization", `Bearer ${await jwt(authenticatedUser.id)}`)
+    //         .expect("Content-Type", /json/)
+    //         .expect(200)
+    //         .then((data) => {
+    //             expect(data.body.data.length).toBe(2);
+    //         });
+    // });
+
+    // it("Admin user should find groups", async () => {
+    //     await request(strapi.server.httpServer)
+    //         .get("/api/groups")
+    //         .set("accept", "application/json")
+    //         .set("Authorization", `Bearer ${await jwt(adminUser.id)}`)
+    //         .expect("Content-Type", /json/)
+    //         .expect(200)
+    //         .then((data) => {
+    //             expect(data.body.data.length).toBe(2);
+    //         });
+    // });
+
+    // it("Public user should find a group", async () => {
+    //     const id = 1;
+    //     const group = constructGroup(id);
+    //     await request(strapi.server.httpServer)
+    //         .get("/api/groups/" + 1)
+    //         .set("accept", "application/json")
+    //         .expect("Content-Type", /json/)
+    //         .expect(200)
+    //         .then((data) => {
+    //             data = data.body.data;
+    //             expect(data.attributes.Name).toBe(group.data.Name);
+    //             expect(data.attributes.ShortDescription).toBe(group.data.ShortDescription);
+    //             expect(data.attributes.locale).toBe(group.data.locale);
+    //         })
+    // });
+
+    // it("Authenticated user should find a group", async () => {
+    //     const id = 1;
+    //     const group = constructGroup(id);
+    //     await request(strapi.server.httpServer)
+    //         .get("/api/groups/" + 1)
+    //         .set("accept", "application/json")
+    //         .set("Authorization", `Bearer ${await jwt(authenticatedUser.id)}`)
+    //         .expect("Content-Type", /json/)
+    //         .expect(200)
+    //         .then((data) => {
+    //             data = data.body.data;
+    //             expect(data.attributes.Name).toBe(group.data.Name);
+    //             expect(data.attributes.ShortDescription).toBe(group.data.ShortDescription);
+    //             expect(data.attributes.locale).toBe(group.data.locale);
+    //         })
+    // });
+
+    // it("Admin user should find a group", async () => {
+    //     const id = 1;
+    //     const group = constructGroup(id);
+    //     await request(strapi.server.httpServer)
+    //         .get("/api/groups/" + 1)
+    //         .set("accept", "application/json")
+    //         .set("Authorization", `Bearer ${await jwt(adminUser.id)}`)
+    //         .expect("Content-Type", /json/)
+    //         .expect(200)
+    //         .then((data) => {
+    //             data = data.body.data;
+    //             expect(data.attributes.Name).toBe(group.data.Name);
+    //             expect(data.attributes.ShortDescription).toBe(group.data.ShortDescription);
+    //             expect(data.attributes.locale).toBe(group.data.locale);
+    //         })
+    // });
+>>>>>>> afbec58 (created test index.js for support)
 })
--- backend/tests/app.test.js
+++ backend/tests/app.test.js
@@ -14,13 +14,11 @@ it("strapi is defined", () => {
   expect(strapi).toBeDefined();
 });
 
+
 require('./api/banner');
 require('./api/group');
-<<<<<<< HEAD
 require('./api/leader');
 require('./api/user');
 require('./api/user-info');
-=======
-require('./api/user');
 require('./api/support');
->>>>>>> 28832a0 (created test index.js for support)
+
