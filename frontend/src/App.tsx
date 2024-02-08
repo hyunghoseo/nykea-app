4841f86db8c4bcab6a592cdc42f1e4f70b6d2f04
Hyungho Seo <hyunghoseo@gmail.com>
2024-02-08 16:49:27 -0500

    KEA-156 fix import order in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
-import { useCallback } from "react";
-import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
-import { View, StyleSheet } from "react-native";
-import { useFonts } from "expo-font";
 import {
   KumbhSans_500Medium,
   KumbhSans_600SemiBold,
 } from "@expo-google-fonts/kumbh-sans";
 import { NotoSans_400Regular } from "@expo-google-fonts/noto-sans";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+import { View, StyleSheet } from "react-native";
 
 import Header from "./components/Header/Header";
 import AppNavigator from "./navigation/AppNavigator";
