18c7b15214c1ec14c1e5e9c5fb541e228a4e854a
jihyunahn <ahn990326@gmail.com>
2024-02-08 15:09:59 -0800

    created announcement file

---
+++ backend/tests/api/announcement/index.js
@@ -0,0 +1,52 @@
+const request = require('supertest');
+const { jwt } = require("../../helpers/strapi");
+const userFactory = require("../../user/factory");
+
+describe("Announcement Test", () => {
+    let authenticatedUser;
+    let adminUser;
+
+    beforeAll(async () => {
+        authenticatedUser = await userFactory.createUser(strapi, "authenticated");
+        adminUser = await userFactory.createUser(strapi, "admin");
+    })
+
+    const constructAnnouncement = (id) => {
+        return {
+            data: {
+                "Name": "Test Announcement " + id,
+                "Poster": null,
+                "Description": "Announcement Description " + id,
+                "HostingGroup": "Announcement HostingGroup" + id,
+                "Private": false,
+                "PastingDate": Date.now(),
+
+
+                "locale": "en",
+                "publishedAt": Date.now(),
+            }
+        };
+    }
+
+    // Get
+    it("[Announcement] Get All Announcements", async () => {
+        await request(strapi.server.httpServer)
+            .get("/api/announcements")
+            .set("accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(200)
+            
+    })
+
+    // Post
+    it("[Announcement] Public user should not post group", async () => {
+        await request(strapi.server.httpServer)
+            .post("/api/announcements")
+            .set("accept", "application/json")
+            .send(constructAnnouncement(0))
+            .expect("Content-Type", /json/)
+            .expect(500)
+    })
+
+})
+
