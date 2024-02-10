48963803b492c71d1a9ade7b4b1467da644b9934
Calvin Yoon <calvinxproject@gmail.com>
2024-02-09 23:40:53 -0500

    KEA-147 drafted all tests

--- backend/tests/api/support/index.js
+++ backend/tests/api/support/index.js
@@ -34,123 +34,137 @@ describe("Support Test", () => {
 
     it("Public user should not post group", async () => {
         await request(strapi.server.httpServer)
-            .post("/api/groups")
+            .post("/api/supports")
             .set("accept", "application/json")
-            .send(constructGroup(0))
-            .expect("Content-Type", /json/)
-            .expect(500)
-    })
-
-    it("Authenticated user should not post group", async () => {
-        await request(strapi.server.httpServer)
-            .post("/api/groups")
-            .set("accept", "application/json")
-            .set("Authorization", `Bearer ${await jwt(authenticatedUser.id)}`)
-            .send(constructGroup(0))
-            .expect("Content-Type", /json/)
-            .expect(500)
-    })
-
-    it("Admin user should post groups", async () => {
-        await request(strapi.server.httpServer)
-            .post("/api/groups")
-            .set("accept", "application/json")
-            .set("Authorization", `Bearer ${await jwt(adminUser.id)}`)
-            .send(constructGroup(1))
-            .expect("Content-Type", /json/)
+            .send(constructSupportTicket(1))
+            .expect('Content-Type',/json/)
             .expect(200)
-
-        await request(strapi.server.httpServer)
-            .post("/api/groups")
-            .set("accept", "application/json")
-            .set("Authorization", `Bearer ${await jwt(adminUser.id)}`)
-            .send(constructGroup(1))
-            .expect("Content-Type", /json/)
-            .expect(200)
-    })
-
-    it("Public user should find groups", async () => {
-        await request(strapi.server.httpServer)
-            .get("/api/groups")
-            .set("accept", "application/json")
-            .expect("Content-Type", /json/)
-            .expect(200)
-            .then((data) => {
-                expect(data.body.data.length).toBe(2);
-            });
     });
 
-    it("Authenticated user should find groups", async () => {
-        await request(strapi.server.httpServer)
-            .get("/api/groups")
-            .set("accept", "application/json")
-            .set("Authorization", `Bearer ${await jwt(authenticatedUser.id)}`)
-            .expect("Content-Type", /json/)
-            .expect(200)
-            .then((data) => {
-                expect(data.body.data.length).toBe(2);
-            });
-    });
+    /**
+        Admin:
+        [] create - done
+        [x] find - done
+        [x] findOne 
+        [x] update 
+        AuthUser:
+        [x] create - done
+        [] find - done
+      
+        Public:
+        [x] create - done
+        [] find - done
 
-    it("Admin user should find groups", async () => {
-        await request(strapi.server.httpServer)
-            .get("/api/groups")
-            .set("accept", "application/json")
-            .set("Authorization", `Bearer ${await jwt(adminUser.id)}`)
-            .expect("Content-Type", /json/)
-            .expect(200)
-            .then((data) => {
-                expect(data.body.data.length).toBe(2);
-            });
-    });
+          **/
 
-    it("Public user should find a group", async () => {
-        const id = 1;
-        const group = constructGroup(id);
-        await request(strapi.server.httpServer)
-            .get("/api/groups/" + 1)
-            .set("accept", "application/json")
-            .expect("Content-Type", /json/)
-            .expect(200)
-            .then((data) => {
-                data = data.body.data;
-                expect(data.attributes.Name).toBe(group.data.Name);
-                expect(data.attributes.ShortDescription).toBe(group.data.ShortDescription);
-                expect(data.attributes.locale).toBe(group.data.locale);
-            })
-    });
+        it("Public user should post support ticket", async () => {
+            await request(strapi.server.httpServer)
+                .post("/api/supports")
+                .set("accept", "application/json")
+                .send(constructSupportTicket(0))
+                .expect('Content-Type',/json/)
+                .expect(200)
+    
+            await request(strapi.server.httpServer)
+                .post("/api/supports")
+                .set("accept", "application/json")
+                .send(constructSupportTicket(1))
+                .expect("Content-Type", /json/)
+                .expect(200)
+        });
 
-    it("Authenticated user should find a group", async () => {
-        const id = 1;
-        const group = constructGroup(id);
-        await request(strapi.server.httpServer)
-            .get("/api/groups/" + 1)
-            .set("accept", "application/json")
-            .set("Authorization", `Bearer ${await jwt(authenticatedUser.id)}`)
-            .expect("Content-Type", /json/)
-            .expect(200)
-            .then((data) => {
-                data = data.body.data;
-                expect(data.attributes.Name).toBe(group.data.Name);
-                expect(data.attributes.ShortDescription).toBe(group.data.ShortDescription);
-                expect(data.attributes.locale).toBe(group.data.locale);
-            })
-    });
+        it("Authenticated user should post support ticket", async () => {
+            await request(strapi.server.httpServer)
+                .post("/api/supports")
+                .set("accept", "application/json")
+                .set("Authorization", `Bearer ${await jwt(authenticatedUser.id)}`)
+                .send(constructSupportTicket(2))
+                .expect("Content-Type", /json/)
+                .expect(200)
+        })
 
-    it("Admin user should find a group", async () => {
-        const id = 1;
-        const group = constructGroup(id);
-        await request(strapi.server.httpServer)
-            .get("/api/groups/" + 1)
-            .set("accept", "application/json")
-            .set("Authorization", `Bearer ${await jwt(adminUser.id)}`)
-            .expect("Content-Type", /json/)
-            .expect(200)
-            .then((data) => {
-                data = data.body.data;
-                expect(data.attributes.Name).toBe(group.data.Name);
-                expect(data.attributes.ShortDescription).toBe(group.data.ShortDescription);
-                expect(data.attributes.locale).toBe(group.data.locale);
-            })
-    });
+        it("Admin user should not post support ticket", async () => {
+            await request(strapi.server.httpServer)
+                .post("/api/supports")
+                .set("accept", "application/json")
+                .set("Authorization", `Bearer ${await jwt(adminUser.id)}`)
+                .send(constructSupportTicket(3))
+                .expect("Content-Type", /json/)
+                .expect(500)
+        })
+
+        it("Public user should not find support tickets", async () => {
+            await request(strapi.server.httpServer)
+                .get("/api/supports")
+                .set("accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(500)
+        })
+
+        it("Authenticated user should not find support tickets", async () => {
+            await request(strapi.server.httpServer)
+                .get("/api/supports")
+                .set("accept", "application/json")
+                .set("Authorization", `Bearer ${await jwt(authenticatedUser.id)}`)
+                .expect("Content-Type", /json/)
+                .expect(500)
+        })
+
+        it("Admin user should find support tickets", async () => {
+            await request(strapi.server.httpServer)
+                .get("/api/supports")
+                .set("accept", "application/json")
+                .set("Authorization", `Bearer ${await jwt(adminUser.id)}`)
+                .expect("Content-Type", /json/)
+                .expect(200)
+                .then((data) => {
+                    expect(data.body.data.length).toBe(4);
+                });
+        });
+
+        it("Admin user should find a support ticket", async () => {
+            const id = 3
+            const support = constructSupportTicket(id);
+            await request(strapi.server.httpServer)
+                .get("/api/supports/" + id)
+                .set("accept", "application/json")
+                .set("Authorization", `Bearer ${await jwt(adminUser.id)}`)
+                .expect("Content-Type", /json/)
+                .expect(200)
+                .then((data) => {
+                    data = data.body.data;
+                    expect(data.attributes.FullName).toBe(support.data.FullName); //expected name3 received name1 why??
+                    expect(data.attributes.Email).toBe(support.data.Email);
+                    expect(data.attributes.Phone).toBe(support.data.Phone);
+                    expect(data.attributes.Title).toBe(support.data.Title);
+                    expect(data.attributes.Message).toBe(support.data.Message);
+                    expect(data.attributes.Status).toBe(support.data.Status);
+                    expect(data.attributes.Memo).toBe(support.data.Memo);
+                    expect(data.attributes.locale).toBe(support.data.locale);
+                })
+        });
+
+        it("Admin user should update a support ticket", async () => {
+            const id = 4; 
+            const updatedStatus = "Completed";
+            const updatedMemo = "Ticket Completed";
+            await request(strapi.server.httpServer)
+                .put("/api/supports/" + id)
+                .set("accept", "application/json")
+                .set("Authorization", `Bearer ${await jwt(adminUser.id)}`)
+                .send({
+                    "data": {
+                        "Status": updatedStatus,
+                        "Memo": updatedMemo
+                    }
+                })
+                .expect("Content-Type", /json/)
+                .expect(200)
+                .then((data) => {
+                    data = data.body.data;
+                    expect(data.attributes.Status).toBe(updatedStatus);
+                    expect(data.attributes.Memo).toBe(updatedMemo);
+                });
+        });
+        
 })
