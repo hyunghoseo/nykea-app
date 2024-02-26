f7576aabbc6ee97437fbd1c21e8ed9f4182062b8
jihyunahn <ahn990326@gmail.com>
2024-02-26 14:58:30 -0800

    test file all passed

--- backend/tests/api/event/index.js
+++ backend/tests/api/event/index.js
@@ -30,5 +30,192 @@ describe("Event Test", () => {
         };
     }
 
+    // Create
+    it("[Create] Public user should not post Events", async () => {
+        await request(strapi.server.httpServer)
+            .post("/api/events")
+            .set("accept", "application/json")
+            .send(constructEvent(0))
+            .expect("Content-Type", /json/)
+            .expect(500)
+    })
+
+    it("[Create] Authenticated user should not post Events", async () => {
+        await request(strapi.server.httpServer)
+            .post("/api/events")
+            .set("accept", "application/json")
+            .set("Authorization", `Bearer ${await jwt(authenticatedUser.id)}`)
+            .send(constructEvent(0))
+            .expect("Content-Type", /json/)
+            .expect(500)
+    })
+
+    it("[Create] Admin user should post Events", async () => {
+        const event = constructEvent(0);
+        await request(strapi.server.httpServer)
+            .post("/api/events")
+            .set("accept", "application/json")
+            .set("Authorization", `Bearer ${await jwt(adminUser.id)}`)
+            .send(constructEvent(0))
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .expect((data) => {
+                data = data.body.data;
+                expect(data.attributes.Title).toBe(event.data.Title);
+                // expected "null", but received "undefined"
+                // expect(data.attributes.Poster).toBe(event.data.Poster);
+                // expect(data.attributes.Picture).toBe(event.data.Picture);
+                expect(data.attributes.Description).toBe(event.data.Description);
+                expect(data.attributes.Location).toBe(event.data.Location);
+                expect(data.attributes.Fee).toBe(event.data.Fee);
+                expect(data.attributes.Contact).toBe(event.data.Contact);
+                expect(data.attributes.Private).toBe(event.data.Private);
+                expect(data.attributes.locale).toBe(event.data.locale);
+            })
+    })
+
+    // Find
+    it("[Find] Public user should find Events", async () => {
+        await request(strapi.server.httpServer)
+            .get("/api/events")
+            .set("accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .then((data) => {
+                expect(data.body.data.length).toBe(1);
+            })
+    })
+
+    it("[Find] Authenticated user should find Events", async () => {
+        await request(strapi.server.httpServer)
+            .get("/api/events")
+            .set("Authorization", `Bearer ${await jwt(authenticatedUser.id)}`)
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .then((data) => {
+                expect(data.body.data.length).toBe(1);
+            })
+    })
+
+    it("[Find] Admin user should find Events", async () => {
+        await request(strapi.server.httpServer)
+            .get("/api/events")
+            .set("Authorization", `Bearer ${await jwt(adminUser.id)}`)
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .then((data) => {
+                expect(data.body.data.length).toBe(1);
+            })
+    })
+
+    // FindOne
+    it("[FindOne] Public user should find an event", async () => {
+        const id = 0;
+        const event = constructEvent(id);
+        await request(strapi.server.httpServer)
+            .get("/api/events/" + 1)
+            .set("accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .then((data) => {
+                data = data.body.data;
+                expect(data.attributes.Title).toBe(event.data.Title);
+                expect(data.attributes.Description).toBe(event.data.Description);
+                expect(data.attributes.Location).toBe(event.data.Location);
+                expect(data.attributes.Fee).toBe(event.data.Fee);
+                expect(data.attributes.Contact).toBe(event.data.Contact);
+                expect(data.attributes.Private).toBe(event.data.Private);
+                expect(data.attributes.locale).toBe(event.data.locale);
+            })
+    })
+
+    it("[FindOne] Authenticated user should find an event", async () => {
+        const id = 0;
+        const event = constructEvent(id);
+        await request(strapi.server.httpServer)
+            .get("/api/events/" + 1)
+            .set("accept", "application/json")
+            .set("Authorization", `Bearer ${await jwt(authenticatedUser.id)}`)
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .then((data) => {
+                data = data.body.data;
+                expect(data.attributes.Title).toBe(event.data.Title);
+                expect(data.attributes.Description).toBe(event.data.Description);
+                expect(data.attributes.Location).toBe(event.data.Location);
+                expect(data.attributes.Fee).toBe(event.data.Fee);
+                expect(data.attributes.Contact).toBe(event.data.Contact);
+                expect(data.attributes.Private).toBe(event.data.Private);
+                expect(data.attributes.locale).toBe(event.data.locale);
+            })
+    })
+
+    it("[FindOne] Admin user should find an event", async () => {
+        const id = 0;
+        const event = constructEvent(id);
+        await request(strapi.server.httpServer)
+            .get("/api/events/" + 1)
+            .set("accept", "application/json")
+            .set("Authorization", `Bearer ${await jwt(adminUser.id)}`)
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .then((data) => {
+                data = data.body.data;
+                expect(data.attributes.Title).toBe(event.data.Title);
+                expect(data.attributes.Description).toBe(event.data.Description);
+                expect(data.attributes.Location).toBe(event.data.Location);
+                expect(data.attributes.Fee).toBe(event.data.Fee);
+                expect(data.attributes.Contact).toBe(event.data.Contact);
+                expect(data.attributes.Private).toBe(event.data.Private);
+                expect(data.attributes.locale).toBe(event.data.locale);
+            })
+    })
+
+    // Update
+    it("[Update] Public user should not update Events", async () => {
+        const id = 1;
+        await request(strapi.server.httpServer)
+            .put("/api/events/" + id)
+            .set("accept", "application/json")
+            .set('Content-Type', 'application/json')
+            .send(constructEvent(10))
+            .expect("Content-Type", /json/)
+            .expect(500)
+    })
+
+    it("[Update] Authenticated user should not update Events", async () => {
+        const id = 1;
+        await request(strapi.server.httpServer)
+            .put("/api/events/" + id)
+            .set("Authorization", `Bearer ${await jwt(authenticatedUser.id)}`)
+            .set('Content-Type', 'application/json')
+            .send(constructEvent(12))
+            .expect("Content-Type", /json/)
+            .expect(500)
+    })
+
+    it("[Update] Admin user should update Events", async () => {
+        const id = 1;
+        const event = constructEvent(13);
+        await request(strapi.server.httpServer)
+            .put("/api/events/" + id)
+            .set("accept", "application/json")
+            .set("Authorization", `Bearer ${await jwt(adminUser.id)}`)
+            .send(event)
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .then((data) => {
+                data = data.body.data;
+                console.log(data.attributes.Title);
+                expect(data.attributes.Title).toBe(event.data.Title);
+                expect(data.attributes.Description).toBe(event.data.Description);
+                expect(data.attributes.Location).toBe(event.data.Location);
+                expect(data.attributes.Fee).toBe(event.data.Fee);
+                expect(data.attributes.Contact).toBe(event.data.Contact);
+                expect(data.attributes.Private).toBe(event.data.Private);
+                expect(data.attributes.locale).toBe(event.data.locale);
+            })
+    })
+
     
 })
