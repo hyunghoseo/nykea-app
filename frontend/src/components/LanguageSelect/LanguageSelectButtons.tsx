b78f291d593ee7bb302256305836d735e2955f28
Hyungho Seo <hyunghoseo@gmail.com>
2024-02-29 20:45:22 -0500

    KEA-83 change language select options to use fontFamily based on the language it shows rather than the language set in the app

--- frontend/src/components/LanguageSelect/LanguageSelectButtons.tsx
+++ frontend/src/components/LanguageSelect/LanguageSelectButtons.tsx
@@ -8,7 +8,6 @@ import { theme } from "@/config/theme";
 import { useLocale } from "@/contexts/LocaleProvider";
 
 const LanguageButton: React.FC<LanguageOption> = (languageOption) => {
-  const styles = useStyles();
   const ref = useRef<TouchableOpacity>(null);
   const isHovered = useHover(ref);
 
@@ -27,13 +26,19 @@ const LanguageButton: React.FC<LanguageOption> = (languageOption) => {
       ]}
     >
       <Image source={languageOption.flag} style={styles.flag} />
-      <Text style={styles.text}>{languageOption.label}</Text>
+      <Text
+        style={[
+          styles.text,
+          languageOption.locale === "en" ? styles.textEn : styles.textKo,
+        ]}
+      >
+        {languageOption.label}
+      </Text>
     </TouchableOpacity>
   );
 };
 
 export const LanguageSelectButtons: React.FC = () => {
-  const styles = useStyles();
   return (
     <View style={styles.container}>
       {languageOptions.map((option) => (
@@ -43,47 +48,47 @@ export const LanguageSelectButtons: React.FC = () => {
   );
 };
 
-const useStyles = () => {
-  const { locale } = useLocale();
-
-  return StyleSheet.create({
-    container: {
-      flexDirection: "row",
-      gap: 32,
-    },
-    button: {
-      height: 40,
-      flexDirection: "row",
-      borderColor: "#EAEAEA",
-      borderWidth: 1,
-      borderRadius: 4,
-      padding: 8,
-      alignItems: "center",
-      ...Platform.select({
-        web: {
-          cursor: "pointer",
-        },
-      }),
-    },
-    buttonHovered: {
-      backgroundColor: theme.colors.primary[8],
-    },
-    buttonActive: {
-      backgroundColor: theme.colors.primary[9],
-    },
-    flag: {
-      width: 24,
-      height: 24,
-    },
-    icon: {},
-    text: {
-      color: "#595959",
-      fontFamily:
-        locale === "en" ? "KumbhSans_500Medium" : "NotoSansKR_500Medium",
-      fontSize: 13,
-      lineHeight: 18,
-      letterSpacing: 0.25,
-      paddingHorizontal: 4,
-    },
-  });
-};
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    gap: 32,
+  },
+  button: {
+    height: 40,
+    flexDirection: "row",
+    borderColor: "#EAEAEA",
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 8,
+    alignItems: "center",
+    ...Platform.select({
+      web: {
+        cursor: "pointer",
+      },
+    }),
+  },
+  buttonHovered: {
+    backgroundColor: theme.colors.primary[8],
+  },
+  buttonActive: {
+    backgroundColor: theme.colors.primary[9],
+  },
+  flag: {
+    width: 24,
+    height: 24,
+  },
+  icon: {},
+  text: {
+    color: "#595959",
+    fontSize: 13,
+    lineHeight: 18,
+    letterSpacing: 0.25,
+    paddingHorizontal: 4,
+  },
+  textEn: {
+    fontFamily: "KumbhSans_500Medium",
+  },
+  textKo: {
+    fontFamily: "NotoSansKR_500Medium",
+  },
+});
--- frontend/src/components/LanguageSelect/LanguageSelectDropdown.tsx
+++ frontend/src/components/LanguageSelect/LanguageSelectDropdown.tsx
@@ -11,19 +11,22 @@ import ChevronDown from "@/assets/chevron-down.svg";
 import Minus from "@/assets/minus.svg";
 
 const DropdownItem: React.FC<LanguageOption> = (languageOption) => {
-  const styles = useStyles();
+  const { locale } = useLocale();
   const ref = useRef<View>(null);
   const isHovered = useHover(ref);
 
   return (
     <View ref={ref} style={[styles.item, isHovered && styles.itemHovered]}>
       <Image source={languageOption.flag} style={styles.flag} />
-      <Text style={styles.text}>{languageOption.label}</Text>
+      <Text
+        style={[styles.text, locale === "en" ? styles.textEn : styles.textKo]}
+      >
+        {languageOption.label}
+      </Text>
     </View>
   );
 };
 export const LanguageSelectDropdown: React.FC = () => {
-  const styles = useStyles();
   const { locale, setLocale } = useLocale();
   const [data, setData] = useState(languageOptions);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -53,14 +56,19 @@ export const LanguageSelectDropdown: React.FC = () => {
       <ChevronDown style={styles.icon} />
     );
 
+  const textStyles = [
+    styles.text,
+    locale === "en" ? styles.textEn : styles.textKo,
+  ];
+
   return (
     <TouchableOpacity activeOpacity={0.6} style={styles.container}>
       <Dropdown
         style={styles.dropdown}
         containerStyle={styles.dropdownMenu}
         itemContainerStyle={styles.dropdownItem}
-        selectedTextStyle={styles.text}
-        placeholderStyle={styles.text}
+        selectedTextStyle={textStyles}
+        placeholderStyle={textStyles}
         iconStyle={{ height: 24, width: 24 }}
         activeColor="#E9EFF6"
         data={data}
@@ -80,58 +88,59 @@ export const LanguageSelectDropdown: React.FC = () => {
   );
 };
 
-const useStyles = () => {
-  const { locale } = useLocale();
-  return StyleSheet.create({
-    container: {
-      flex: 1,
-      ...Platform.select({
-        web: {
-          cursor: "pointer",
-        },
-      }),
-    },
-    dropdown: {
-      height: 40,
-      width: 124,
-      borderColor: "#EAEAEA",
-      borderWidth: 1,
-      borderRadius: 4,
-      // padding: 8,
-    },
-    dropdownMenu: {
-      borderRadius: 4,
-      borderWidth: 0,
-      shadowColor: "#000",
-      shadowOffset: { width: 0, height: 2 },
-      shadowOpacity: 0.23,
-      shadowRadius: 2.62,
-      elevation: 4, // for Android
-    },
-    dropdownItem: {
-      // padding: 8,
-    },
-    item: {
-      flexDirection: "row",
-      alignItems: "center",
-      height: 40,
-    },
-    itemHovered: {
-      backgroundColor: theme.colors.primary[9],
-    },
-    flag: {
-      width: 24,
-      height: 24,
-      margin: 8,
-    },
-    icon: { margin: 8 },
-    text: {
-      color: "#595959",
-      fontFamily:
-        locale === "en" ? "KumbhSans_500Medium" : "NotoSansKR_500Medium",
-      fontSize: 13,
-      lineHeight: 18,
-      letterSpacing: 0.25,
-    },
-  });
-};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    ...Platform.select({
+      web: {
+        cursor: "pointer",
+      },
+    }),
+  },
+  dropdown: {
+    height: 40,
+    width: 124,
+    borderColor: "#EAEAEA",
+    borderWidth: 1,
+    borderRadius: 4,
+    // padding: 8,
+  },
+  dropdownMenu: {
+    borderRadius: 4,
+    borderWidth: 0,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4, // for Android
+  },
+  dropdownItem: {
+    // padding: 8,
+  },
+  item: {
+    flexDirection: "row",
+    alignItems: "center",
+    height: 40,
+  },
+  itemHovered: {
+    backgroundColor: theme.colors.primary[9],
+  },
+  flag: {
+    width: 24,
+    height: 24,
+    margin: 8,
+  },
+  icon: { margin: 8 },
+  text: {
+    color: "#595959",
+    fontSize: 13,
+    lineHeight: 18,
+    letterSpacing: 0.25,
+  },
+  textEn: {
+    fontFamily: "KumbhSans_500Medium",
+  },
+  textKo: {
+    fontFamily: "NotoSansKR_500Medium",
+  },
+});
