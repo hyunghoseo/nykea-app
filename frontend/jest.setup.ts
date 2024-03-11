27d86edabe0deee422bdc6fdfd7b62be4d7262ca
Hyungho Seo <hyunghoseo@gmail.com>
2024-03-11 19:58:53 -0400

    KEA-36 mock Skeleton component

--- frontend/jest.setup.ts
+++ frontend/jest.setup.ts
@@ -21,3 +21,7 @@ jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
 jest.mock("@react-native-async-storage/async-storage", () => mockAsyncStorage);
 
 jest.mock("react-native-safe-area-context", () => mockSafeAreaContext);
+
+jest.mock("moti/skeleton", () => ({
+  Skeleton: () => "Skeleton",
+}));
