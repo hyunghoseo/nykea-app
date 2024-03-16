f79068ba33ba4d0eb3d9e1369f68fd322c5906f6
Calvin Yoon <122571885+calvinyoon@users.noreply.github.com>
2024-03-16 08:56:14 -0400

    KEA-149 As an admin, I want to manage basic church information so that I can easily make changes on the information (#37)
    * created church-info index file
    
    * started test code & set admin permissions for church-info strapi
    
    * reverting Strapi content manager changes
    
    * disabled delete church-info privilege for admin
    
    * revised phone input
    
    * drafted tests for church-info
    
    * config sync - import
    
    * removed console log
    
    * returned size from 6 to 12
    
    * disabled localization for private policy
    
    * KEA-149 reverted changes to unrelated api group.json

---
+++ backend/tests/api/church-info/index.js
@@ -0,0 +1,118 @@
+const request = require('supertest');
+const { jwt } = require("../../helpers/strapi");
+const userFactory = require("../../user/factory");
+
+describe("Church Info Test", () => {
+    let authenticatedUser;
+    let adminUser;
+    beforeAll(async () => {
+        authenticatedUser = await userFactory.createUser(strapi, "authenticated");
+        adminUser = await userFactory.createUser(strapi, "admin");
+    });
+
+    /**
+     * @param {string | number} id
+     */
+
+    const constructChurchInfo = (id) => {
+        return {
+            data: {
+                "ChurchName": "Test Name " + id,
+                "Phone": "000-000-0000",
+                "Email": "test" + id + "@test.com",
+                "Address": id + "north haledon",
+                "PrivatePolicy": "policy" + id,
+                "locale": "en",
+                "publishedAt": Date.now(),
+            }
+        };
+    }
+
+    it("Public user should not put church-info", async () => {
+        await request(strapi.server.httpServer)
+            .put("/api/church-info") //strapi uses put for single type
+            .set("accept", "application/json")
+            .send(constructChurchInfo(0))
+            .expect("Content-Type", /json/)
+            .expect(500)
+    });
+
+    it("Authenticated user should not put church-info", async () => {
+        await request(strapi.server.httpServer)
+            .put("/api/church-info") //strapi uses put for single type
+            .set("accept", "application/json")
+            .set("Authorization", `Bearer ${await jwt(authenticatedUser.id)}`)
+            .send(constructChurchInfo(0))
+            .expect("Content-Type", /json/)
+            .expect(500)
+    });
+
+    it("Admin user should put church-info", async () => {
+        await request(strapi.server.httpServer)
+            .put("/api/church-info") //strapi uses put for single type
+            .set("accept", "application/json")
+            .set("Authorization", `Bearer ${await jwt(adminUser.id)}`)
+            .send(constructChurchInfo(1))
+            .expect("Content-Type", /json/)
+            .expect(200)   
+    });
+
+    it("Public user should find church-info", async () => {
+        const id = 1;
+        const churchInfo = constructChurchInfo(id);
+        await request(strapi.server.httpServer)
+            .get("/api/church-info")
+            .set("accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .then((data) => {
+                data = data.body.data;
+                expect(data.attributes.ChurchName).toBe(churchInfo.data.ChurchName);
+                expect(data.attributes.Phone).toBe(churchInfo.data.Phone);
+                expect(data.attributes.Email).toBe(churchInfo.data.Email);
+                expect(data.attributes.Address).toBe(churchInfo.data.Address);
+                expect(data.attributes.PrivatePolicy).toBe(churchInfo.data.PrivatePolicy);
+                expect(data.attributes.locale).toBe(churchInfo.data.locale);
+            });
+    });
+
+    it("Authenticated user should find church-info", async () => {
+        const id = 1;
+        const churchInfo = constructChurchInfo(id);
+        await request(strapi.server.httpServer)
+            .get("/api/church-info")
+            .set("accept", "application/json")
+            .set("Authorization", `Bearer ${await jwt(authenticatedUser.id)}`)
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .then((data) => {
+                data = data.body.data;
+                expect(data.attributes.ChurchName).toBe(churchInfo.data.ChurchName);
+                expect(data.attributes.Phone).toBe(churchInfo.data.Phone);
+                expect(data.attributes.Email).toBe(churchInfo.data.Email);
+                expect(data.attributes.Address).toBe(churchInfo.data.Address);
+                expect(data.attributes.PrivatePolicy).toBe(churchInfo.data.PrivatePolicy);
+                expect(data.attributes.locale).toBe(churchInfo.data.locale);
+            });
+    });
+
+    it("Admin user should find church-info", async () => {
+        const id = 1;
+        const churchInfo = constructChurchInfo(id);
+        await request(strapi.server.httpServer)
+            .get("/api/church-info")
+            .set("accept", "application/json")
+            .set("Authorization", `Bearer ${await jwt(adminUser.id)}`)
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .then((data) => {
+                data = data.body.data;
+                expect(data.attributes.ChurchName).toBe(churchInfo.data.ChurchName);
+                expect(data.attributes.Phone).toBe(churchInfo.data.Phone);
+                expect(data.attributes.Email).toBe(churchInfo.data.Email);
+                expect(data.attributes.Address).toBe(churchInfo.data.Address);
+                expect(data.attributes.PrivatePolicy).toBe(churchInfo.data.PrivatePolicy);
+                expect(data.attributes.locale).toBe(churchInfo.data.locale);
+            });
+    });
+})
