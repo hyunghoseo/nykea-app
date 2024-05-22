663cd2513cd4e34f2ee55c52eeae5510c223337c
jihyunahn <ahn990326@gmail.com>
2024-05-21 23:37:21 -0700

    KEA-145 solve the error

--- backend/tests/helpers/constructor.js
+++ backend/tests/helpers/constructor.js
@@ -28,8 +28,8 @@ const constructAnnouncement = (id) => {
 }
 
 /**
-     * @param {string | number} id
-     */
+ * @param {string | number} id
+ */
 const constructBanner = (id) => {
     return {
         data: {
@@ -60,40 +60,6 @@ const constructChurchInfo = (id) => {
             "PrivatePolicy": "policy" + id,
             "locale": "en",
             "publishedAt": Date.now(),
-        }
-    };
-}
-
-/**
- * @param {string | number} id
- */
-const constructEvent = (id) => {
-    return {
-        data: {
-            "Title": "Event Title " + id,
-            "Poster": null,
-            "StartDate": {
-                "Date": "2024-04-14",
-                "Time": "12:13:54.000",
-                "TimeZone": "EST"
-            },
-            "Location": [
-                {
-                    "Label": "KEA Church 100",
-                    "AddressURL": "www.google.com",
-                    "Address": "374 Squaw Brook Rd",
-                    "AddressDetail": null,
-                    "City": null,
-                    "Country": null,
-                    "state": null
-                }
-            ],
-            "Description": "Event Description " + id,
-            "HostingGroup": null,
-            "Fee": "$ Event Fee " + id,
-            "Contact": "Event Contact " + id,
-            "Private": false,
-            "locale": "en"
         }
     };
 }
@@ -164,44 +130,6 @@ const constructUserInfo = (id) => {
             "users_permissions_user": null,
             "locale": "en",
             "publishedAt": Date.now(),
-        }
-    };
-}
-
-/**
-  * @param {string | number} id
-  * @param {string | number} userInfoId
-  */
-const constructLeader = (id, userInfoId) => {
-    return {
-        data: {
-            "StartYear": 2020,
-            "EndYear": 9999,
-            "Title": "Pastor",
-            "FullName": userInfoId,
-            "Picture": null,
-            "locale": "en",
-            "publishedAt": Date.now(),
-        }
-    };
-}
-
-/**
- * @param {string | number} id
- */
-
-const constructSupportTicket = (id) => {
-    return {
-        data: {
-            "FullName": "Test Name" + id,
-            "Email": "test" + id + "@email.com",
-            "Phone": "111-111-1111",
-            "Title": "Test Title" + id,
-            "Message": "Test Message" + id,
-            "DateSubmitted": Date.now(),
-            "Status": "Submitted", // "Under Review" or "Completed"
-            "Memo": null,
-            "DateCompleted": null,
         }
     };
 }
