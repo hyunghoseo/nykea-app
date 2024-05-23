06670c4878ea98dbd7512819a422fe85a0c43329
doolri1276 <doolri1277@gmail.com>
2024-05-22 21:30:03 -0400

    KEA-194 details page

--- frontend/src/components/Layout/DetailPage.tsx
+++ frontend/src/components/Layout/DetailPage.tsx
@@ -8,40 +8,59 @@ import ScreenWrapper from "../ScreenWrapper";
 import { Tag } from "./Tag";
 
 interface DetailPageProps {
-  type: string;
-  isLoading: boolean;
-  isError: boolean;
-  data: any;
-  title: string;
-  tag1: string;
-  postedDate: string;
-  description: string;
+    type: string;
+    isLoading: boolean;
+    isError: boolean;
+    data: any;
+    title: string;
+    tag1: string;
+    postedDate: string;
+    description: string;
 }
 
 export const DetailPage: React.FC<DetailPageProps> = (props) => {
-  const styles = useStyles();
-  const { h2, h6 } = useTypographyStyles();
-  return (
-    <ScreenWrapper>
-      <View>
-        <View>
-          <View>
-            <Tag text={props.tag1} />
-            <Tag text={props.tag1} />
-          </View>
-          <H2 style={[h2]}>{props.title}</H2>
-          <H6 style={[h6, styles.date]}>{props.postedDate}</H6>
-        </View>
-        <View>{props.data}</View>
-      </View>
-    </ScreenWrapper>
-  );
+    const styles = useStyles();
+    const { h2, h6, bodyNormal } = useTypographyStyles();
+    return (
+        <ScreenWrapper>
+            {props.isError ? (
+                <P style={bodyNormal}>There was an error getting announcement</P>
+            ) : null}
+            {!props.isError && !props.isLoading && props.data ? (
+                <View style={styles.wrapper}>
+                    <View style={styles.headerSection}>
+                        <View style={styles.tags}>
+                            <Tag text={props.tag1} />
+                            <Tag text={props.tag1} />
+                        </View>
+                        <H2 style={[h2]}>{props.data?.attributes?.title}</H2>
+                        <H6 style={[h6, styles.date]}>{props.postedDate}</H6>
+                    </View>
+                    <View>{props.data}</View>
+                </View>
+            ) : (
+                    <View>
+                        <P style={bodyNormal}>Still loading</P>
+                    </View>
+                )}
+        </ScreenWrapper>
+    );
 };
 
 const useStyles = () => {
-  return StyleSheet.create({
-    date: {
-      color: theme.colors.dark,
-    },
-  });
+    return StyleSheet.create({
+        wrapper: {
+            width: '100%',
+            maxWidth: 672,
+        },
+        headerSection: {
+
+        },
+        tags: {
+            flexDirection: 'row'
+        },
+        date: {
+            color: theme.colors.dark,
+        },
+    });
 };
