d7f9b890010ee3689ffcae28503059f0fef7309b
Hyungho Seo <hyunghoseo@gmail.com>
2024-08-06 02:00:52 -0400

    add cloudinary integration

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -58,4 +58,18 @@ module.exports = ({ env }) => ({
       security: [{ bearerAuth: [] }],
     },
   },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET')
+      },
+      actionOptions: {
+        upload: {},
+        delete: {}
+      }
+    }
+  }
 });
