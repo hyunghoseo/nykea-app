f9ef45ff192a1c93f64a97f34aaaf13a5bf976de
Hyungho Seo <hyunghoseo@gmail.com>
2024-09-25 18:22:30 -0400

    KEA-211 Add a tablet responsive breakpoint (#88)

--- frontend/src/config/constants.ts
+++ frontend/src/config/constants.ts
@@ -1,5 +1,6 @@
 export const MOBILE_HEADER_HEIGHT = 84;
 export const DESKTOP_HEADER_HEIGHT = 96;
-export const MOBILE_BREAKPOINT = 1280;
+export const MOBILE_BREAKPOINT = 768;
+export const TABLET_BREAKPOINT = 1280;
 
 export const PLACEHOLDER_BLUR_HASH = "LBQ0w8og~Vt7xufQWCfQ_1j[9Hay";
